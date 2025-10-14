2bdade1d546f46ea58486cefe77ef6f88fcede4c
Adarsh Bharathwaj <adarsh.bharathwaj@outlook.com>
2025-10-13 20:51:58 -0400

    Making player movement more reactive

--- js/main.js
+++ js/main.js
@@ -4,11 +4,25 @@ const main = document.querySelector('main');
 const char = document.querySelector('.nav-character');
 
 let charPos = 0;
+let velocity = 0;
+const friction = 0.8;
+const speed = 2;
+
+function updateChar() {
+    velocity *= friction;
+    charPos += velocity;
+    char.style.transform = `translate(${charPos}px)`;
+    requestAnimationFrame(updateChar);
+}
+
+
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowRight') charPos += 10;
-    if (e.key == 'ArrowLeft') charPos -= 10;
+    if (e.key === 'ArrowRight') velocity = speed;
+    if (e.key == 'ArrowLeft') velocity = -speed;
     char.style.transform = `translateX(${charPos}px)`;
 });
+
+updateChar();
 
 document.querySelectorAll('.navbar a').forEach( anchor => {
     anchor.addEventListener('click', function (e) {
